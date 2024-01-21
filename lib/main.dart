8a71a341eb8bc97c702b7be99f344880e4b801e3
sahilt65 <sahiltpict@gmail.com>
2024-01-21 23:34:55 +0530

    Initialized Firebase for iOS and Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:streaming_app/screens/login_screen.dart';
 import 'package:streaming_app/screens/onboarding_screen.dart';
 import 'package:streaming_app/screens/signup_screen.dart';
 import 'package:streaming_app/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
---
+++ lib/resources/auth_methods.dart
@@ -0,0 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthMethods {
+  final _userRef = FirebaseFirestore.instance.collection('users');
+  final _auth = FirebaseAuth.instance;
+
+  signupUser() async {}
+}
